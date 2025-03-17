bbe20d2a5ac56814baa5b8a3ea4557ec6d4edb9f
mozix5 <mosinmd827@gmail.com>
2025-03-17 15:21:12 +0530

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import AuthLayout from "@/pages/authLayout.tsx";
+import Home from "@/pages/home.tsx";
+import ProtectedRoutes from "@/components/protectedRoutes.tsx";
+
 const App = () => {
-  return <div className="bg-red-500">hello</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
